9c2168e39f880e2eb56826dfc9554e9f2807c9c5
Megagig <megagigsoftwaresolutions.com>
2024-11-18 23:45:30 +0100

    hashedPassword

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,20 +2,27 @@
 const User = require('../models/User');
 const { StatusCodes } = require('http-status-codes');
 const { BadRequestError } = require('../errors');
+const bcrypt = require('bcryptjs');
 
 const Register = async (req, res) => {
   const { name, email, password } = req.body;
 
-  //ERROR HANDLING: 1st way: CONTROLLER ERROR HANDLING
+  // Check if user already exists
+  const existingUser = await User.findOne({ email });
+  if (existingUser) {
+    throw new BadRequestError('Email already in use');
+  }
 
-  //validate if the user has entered all the required fields
-  // if (!name || !email || !password) {
-  //   throw new BadRequestError('Please provide all values');
-  // }
+  //hash the password
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
 
-  const user = await User.create({ ...req.body });
+  //create a new user
 
-  //check if user already exists
+  const user = await User.create({ name, email, password: hashedPassword });
+
+  //return the new user
+
   res.status(StatusCodes.CREATED).json({ user });
 };
 
