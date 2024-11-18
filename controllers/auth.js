911dc337fd0cf33553f384fe315365d8bd4d4bff
Megagig <megagigsoftwaresolutions.com>
2024-11-18 21:27:35 +0100

    Error Handling:Controller Example

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,22 @@
+//import the user model
+const User = require('../models/User');
+const { StatusCodes } = require('http-status-codes');
+const { BadRequestError } = require('../errors');
+
 const Register = async (req, res) => {
-  res.send('Register User');
+  const { name, email, password } = req.body;
+
+  //ERROR HANDLING: 1st way: CONTROLLER ERROR HANDLING
+
+  //validate if the user has entered all the required fields
+  // if (!name || !email || !password) {
+  //   throw new BadRequestError('Please provide all values');
+  // }
+
+  const user = await User.create({ ...req.body });
+
+  //check if user already exists
+  res.status(StatusCodes.CREATED).json({ user });
 };
 
 const Login = async (req, res) => {
