fbabd8488e3bde9c2b394953867a5fd1b950027c
Megagig <megagigsoftwaresolutions.com>
2024-11-18 18:24:33 +0100

    Setup the route for the project

---
+++ routes/jobs.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+
+const {
+  createJob,
+  getJobs,
+  getAllJob,
+  updateJob,
+  deleteJob,
+} = require('../controllers/jobs');
+
+router.post('/', createJob);
+router.get('/:id', getJobs);
+router.get('/all', getAllJob);
+router.patch('/:id', updateJob);
+router.delete('/:id', deleteJob);
+module.exports = router; // Export the router
