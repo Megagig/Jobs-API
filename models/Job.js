46bdcb07f29f039812f0f8c0d2e9420c236d404a
Megagig <megagigsoftwaresolutions.com>
2024-11-19 18:37:15 +0100

    Create the Job Model

---
+++ models/Job.js
@@ -0,0 +1,58 @@
+const mongoose = require('mongoose');
+
+const JobSchema = new mongoose.Schema(
+  {
+    company: {
+      type: String,
+      required: [true, 'Please provide a company name'],
+      maxlength: 50,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    position: {
+      type: String,
+      required: [true, 'Please provide a position'],
+      maxlength: 100,
+    },
+    status: {
+      type: String,
+      enum: ['interview', 'declined', 'pending'],
+      default: 'pending',
+    },
+
+    location: {
+      type: String,
+      required: true,
+    },
+    salary: {
+      type: Number,
+      required: true,
+    },
+    jobType: {
+      type: String,
+      required: true,
+    },
+    experience: {
+      type: String,
+      required: true,
+    },
+    skills: {
+      type: [String],
+      required: true,
+    },
+    date: {
+      type: Date,
+      default: Date.now,
+    },
+    createdBy: {
+      type: mongoose.Types.ObjectId,
+      ref: 'User',
+      required: [true, 'Please provide a user'],
+    }, // user who created the job
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model('Job', JobSchema);
